26ab0a095e137a3e4455018099d9f9464a21349a
ooijingkai <ooijingkai@gmail.com>
2025-03-16 23:46:29 +0800

    feat: update data type for user id and product id

---
+++ genproto.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# PATH=$PATH:$(go env GOPATH)/bin
+protodir=./proto
+outdir=proto
+
+protoc --proto_path=$protodir --go_out=./backend/$outdir --go_opt=paths=source_relative --go-grpc_out=./backend/$outdir --go-grpc_opt=paths=source_relative $protodir/ecommerce.proto
+
+protoc --proto_path=$protodir --go_out=./product/$outdir --go_opt=paths=source_relative --go-grpc_out=./product/$outdir --go-grpc_opt=paths=source_relative $protodir/ecommerce.proto
