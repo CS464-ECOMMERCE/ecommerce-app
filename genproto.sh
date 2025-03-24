403aa82cb6eec7279d74cb93831f9159ca85fadd
ooijingkai <ooijingkai@gmail.com>
2025-03-25 00:47:59 +0800

    fix: grpc server for cart service

--- genproto.sh
+++ genproto.sh
@@ -8,5 +8,8 @@ protoc --proto_path=$protodir --go_out=./backend/$outdir --go_opt=paths=source_r
 
 protoc --proto_path=$protodir --go_out=./product/$outdir --go_opt=paths=source_relative --go-grpc_out=./product/$outdir --go-grpc_opt=paths=source_relative $protodir/ecommerce.proto
 
+protoc --proto_path=$protodir --go_out=./cart/$outdir --go_opt=paths=source_relative --go-grpc_out=./cart/$outdir --go-grpc_opt=paths=source_relative $protodir/ecommerce.proto
+
 protoc-go-inject-tag -input=./backend/$outdir/ecommerce.pb.go
 protoc-go-inject-tag -input=./product/$outdir/ecommerce.pb.go
+protoc-go-inject-tag -input=./cart/$outdir/ecommerce.pb.go
